ee9722556113eb436a9a4b6717abf6fbc0c99aa7
talpitoo <mounttain@gmail.com>
2023-11-16 17:19:09 +0100

    introduced GSAP for the footer/newsletter animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,6 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
     // wait until images, links, fonts, stylesheets, and js is loaded
     window.addEventListener("load", function (e) {
 
+
+
         /**
          * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
          * hamburger
@@ -40,6 +42,31 @@ document.addEventListener("DOMContentLoaded", function () {
                 // NOTE: in case we have a button: document.exitFullscreen();
             });
         }
+
+
+
+        /**
+         * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+         * fade in the newsletter once scrolled into view, using GSAP https://gsap.com/docs/v3/Plugins/ScrollTrigger/
+         * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+         */
+        // set initial state (hidden)
+        gsap.set('#newsletter-wrapper [data-animation="true"]', {
+            opacity: 0.01,
+            xPercent: -50,
+        });
+
+        // animate to visible state
+        gsap.to('#newsletter-wrapper [data-animation="true"]', {
+            scrollTrigger: {
+                trigger: "footer",
+                end: "top center", // end the animation when the top of the footer reaches the center of the viewport
+                scrub: 2, // smoothly animate the opacity as you scroll
+            },
+            stagger: 0.1,
+            opacity: 1,
+            xPercent: 0,
+        });
 
 
 
